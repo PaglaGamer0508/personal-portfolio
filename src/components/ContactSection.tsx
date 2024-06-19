702d2404c5bd5bace4b1c8dd7a479b224acfb0d1
fahad <fahadmahmud225@gmail.com>
2024-06-19 20:55:43 +0600

    contact section text fixed

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -17,7 +17,7 @@ const ContactSection: React.FC<ContactSectionProps> = ({}) => {
         className={`${chivoMono.className} text-xl md:text-2xl flex flex-col gap-y-14 w-[95%] md:w-[60%] lg:w-[45%] mx-auto mt-10`}
       >
         <div>
-          <h1>Country</h1>
+          <h1>Address</h1>
           <Link
             target="_blank"
             href={`https://www.google.com/maps/place/Dhaka/@23.7810672,90.2548734,11z/data=!3m1!4b1!4m6!3m5!1s0x3755b8b087026b81:0x8fa563bbdd5904c2!8m2!3d23.804093!4d90.4152376!16zL20vMGZuYjQ?entry=ttu`}
@@ -29,7 +29,7 @@ const ContactSection: React.FC<ContactSectionProps> = ({}) => {
         </div>
 
         <div>
-          <h1>Country</h1>
+          <h1>Email</h1>
           <Link href={`mailto:fahadmahmud747@gmail.com`}>
             <div className="bg-gray-900 hover:bg-gray-800 transition-all duration-75 rounded-3xl px-6 md:px-10 py-5 mt-5">
               fahadmahmud747@gmail.com
